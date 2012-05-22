3183b9fa5b185208ee046ab34b685f6bc5f101f8
sopyan <delta.paradise@gmail.com>
2012-05-22 09:01:03 +0700

    22 Mei 2012

--- GrafkomSolution/Tugas-Kelompok1/Tugas-Kelompok1.cpp
+++ GrafkomSolution/Tugas-Kelompok1/Tugas-Kelompok1.cpp
@@ -1,11 +1,8 @@
-// Tugas-Kelompok1.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-
-
-// Tugas 6_PembuatanBentuk3D.cpp : Defines the entry point for the console application.
-//
+﻿// Tugas-Kelompok1.cpp : Pembuatan Objek 3D
+//Diki Taurens Sia 10108396
+//Sopyan		   10108400
+//Andika Tanjung   10108405
+//M. Arif Ansyari  10108407
 
 #include "stdafx.h"
 #include <windows.h>
@@ -18,271 +15,200 @@
 #include <gl\GLU.h>
 #include <gl\glut.h>
 
+static float ypoz = 0, zpoz = 0, xpoz = 0,a = 0, b = 0,c = -10;
 
-/*
-void axis(double length)
-{ 
-	glPushMatrix();
-	glBegin(GL_LINES);
-	   glVertex3d(0, 0, 0); glVertex3d(0,0,length); 
-	glEnd();
-	glTranslated(0, 0,length -0.2); 
-	glutWireCone(0.04, 0.2, 12, 9);
-	glPopMatrix();
-}	
-
-void displayWire(void)
+void coba(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4,int z1,float z2,float z3)
 {
-	glMatrixMode(GL_PROJECTION); 
-	glLoadIdentity();
-	glOrtho(-2.0*64/48.0, 2.0*64/48.0, -2.0, 2.0, 0.1, 100);
-	glMatrixMode(GL_MODELVIEW); 
-	glLoadIdentity();
-	gluLookAt(2.0, 2.0, 8.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-	
-	glClear(GL_COLOR_BUFFER_BIT); 
-    glColor3d(0,0,0);			
-
-	glPushMatrix();
-	//glTranslated(0.5, 0.5, 0.5); 
-	glutWireCube(2.0);
-	glPopMatrix();
-
-	glPushMatrix();	
-	//glTranslated(1.0,1.0,0);	
-	glutWireSphere(0.25, 10, 8);
-	glPopMatrix();	
-
-	glPushMatrix();
-	glTranslated(0, 2 ,2);
-	GLUquadricObj * qobj;
-	qobj = gluNewQuadric();
-	gluQuadricDrawStyle(qobj,GLU_LINE);
-	gluCylinder(qobj, 0.2, 0.8, 2, 8, 10);
-	glPopMatrix();
-
-	glFlush();
+	glColor3f(1,0,0); //sisi depan
+	glVertex3d(x1,y1,z2);
+	glVertex3d(x2,y2,z2);
+	glVertex3d(x3,y3,z2);
+	glVertex3d(x4,y4,z2);
+	glColor3f(1,0,0);//right
+	glVertex3d(x2,y2,z2);
+	glVertex3d(x2,y2,z1);
+	glVertex3d(x3,y3,z1);
+	glVertex3d(x3,y3,z2);
+	glColor3f(1,0,0);//back
+	glVertex3d(x1,y1,z1);
+	glVertex3d(x2,y2,z1);
+	glVertex3d(x3,y3,z1);
+	glVertex3d(x4,y4,z1);
+	glColor3f(1,0,0);//left
+	glVertex3d(x1,y1,z2);
+	glVertex3d(x1,y1,z1);
+	glVertex3d(x4,y4,z1);
+	glVertex3d(x4,y4,z2);
+	glColor3f(1,0,1);//bottom
+	glVertex3d(x1,y1,z2);
+	glVertex3d(x2,y2,z2);
+	glVertex3d(x2,y2,z1);
+	glVertex3d(x1,y1,z1);
+	glColor3f(1,1,0);//top
+	glVertex3d(x3,y3,z2);
+	glVertex3d(x4,y4,z2);
+	glVertex3d(x4,y4,z1);
+	glVertex3d(x3,y3,z1);
+}
+void cobain(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4,float z1,float z2,float z3)
+{ //atap
+	glColor3f(1,1,1);//kanan
+	glVertex3d(x3,y3,z1);
+	glVertex3d(x3,y3,z2);
+	glVertex3d(x3,y1,z3);
+	glColor3f(1,1,1);//kiri
+	glVertex3d(x4,y4,z2);
+	glVertex3d(x4,y4,z1);
+	glVertex3d(x4,y1,z3);
+}
+void cobaini(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4,float z1,float z2,float z3)
+{//atap
+	glColor3f(1,0,1);//belakang
+	glVertex3d(x4,y4,z1);
+	glVertex3d(x3,y3,z1);
+	glVertex3d(x3,y1,z3);
+	glVertex3d(x4,y1,z3);
+	glColor3f(1,0,1);//depan
+	glVertex3d(x3,y3,z2);
+	glVertex3d(x4,y4,z2);
+	glVertex3d(x4,y1,z3);
+	glVertex3d(x3,y1,z3);
+}
+void init(void)
+{
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glOrtho(-12,12,-6,6,-35,35);
+	glEnable(GL_DEPTH_TEST);
+	glShadeModel (GL_SMOOTH);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity ();
+	gluPerspective(80.0,2.2, 1.0, 20.0);
+	glMatrixMode (GL_MODELVIEW);
 }
 
-void main(int argc, char **argv)
+void display(void)
+{
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity ();
+	glTranslatef(a,b,c);
+	glRotatef(xpoz,1,0,0);
+	glRotatef(ypoz,0,1,0);
+	glRotatef(zpoz,0,0,1);
+
+	glBegin(GL_QUADS);
+	//Rumah
+	coba(-8,-2,8,-2,8,2,-8,2,-5,3,0);//rumah
+	coba(2,-2,8,-2,8,2,2,2,-5,4,0);//kamar
+	coba(-8,-2,-7,-2,-7,2,-8,2,-5,4,0);//dindingpagar kiri
+	coba(-8,-2,-7,-2,-7,0,-8,0,-5,5.5,0);
+	coba(7,-2,8,-2,8,0,7,0,-5,5.5,0);
+	cobaini(-3,5,0,0,8,2,-8,2,-5.5,4.5,0);
+
+	glColor3f(1,1,1);//plafon depan
+	glVertex3d(8,1.8,-3);
+	glVertex3d(-8,1.8,-3);
+	glVertex3d(-8,1.8,4.5);
+	glVertex3d(8,1.8,4.5);
+	glVertex3d(8,1.8,4.5);
+	glVertex3d(-8,1.8,4.5);
+	glVertex3d(-8,2,4.5);
+	glVertex3d(8,2,4.5);
+
+	glVertex3d(4,-0.5,4.1);//cendela kamar
+	glVertex3d(6,-0.5,4.1);
+	glVertex3d(6,1,4.1);
+	glVertex3d(4,1,4.1);
+	glVertex3d(2.7,-0.5,4.1);//cendela kamar2
+	glVertex3d(3.8,-0.5,4.1);
+	glVertex3d(3.8,1,4.1);
+	glVertex3d(2.7,1,4.1);
+
+	glVertex3d(-2,-2,3.05);//pintu depan
+	glVertex3d(2,-2,3.05);
+	glVertex3d(2,1.3,3.05);
+	glVertex3d(-2,1.3,3.05);
+	glVertex3d(-2,-2,-5.05);//pintu belakang
+	glVertex3d(2,-2,-5.05);
+	glVertex3d(2,1,-5.05);
+	glVertex3d(-2,1,-5.05);
+
+
+	glBegin(GL_TRIANGLES);
+	cobain(-3,5,0,0,8,2,-8,2,-5.5,4.5,0);
+	glEnd();
+
+	glBegin(GL_LINES);
+	glColor3f(0,0,0);
+	glVertex3d(8,1.8,4.5);
+	glVertex3d(-8,1.8,4.5);
+	glVertex3d(-8,1.8,-5.5);
+	glVertex3d(8,1.8,-5.5);
+	glColor3f(1,0,0);
+	glVertex3d(-4.5,-1,-5.05);
+	glVertex3d(-4.5,1,-5.05);
+	glVertex3d(4.5,-1,-5.05);
+	glVertex3d(4.5,1,-5.05);
+	glEnd();
+	glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) 
+	{
+		// These will allow you to force some extra rotation by pressing the 'y' and 'z' key.
+		case 'x':
+		xpoz=xpoz+5;
+		if (xpoz>360) xpoz=0;
+		glutPostRedisplay();
+		break;
+		case 'y':
+		ypoz=ypoz+5;
+		if (ypoz>360) ypoz=0;
+		glutPostRedisplay();
+		break;
+		case 'z':
+		zpoz = zpoz+1;
+		if (zpoz>360) zpoz=0;
+		glutPostRedisplay();
+		break;
+		case 'w':
+		b = b + 1;
+		glutPostRedisplay();
+		break;
+		case 's':
+		b = b - 1;
+		glutPostRedisplay();
+		break;
+		case 'a':
+		a = a + 1;
+		glutPostRedisplay();
+		break;
+		case 'd':
+		a = a - 1;
+		glutPostRedisplay();
+		break;
+		case 'q':
+		c = c + 1;
+		glutPostRedisplay();
+		break;
+		case 'e':
+		c = c - 1;
+		glutPostRedisplay();
+		break;
+
+	}
+}
+int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
-	glutInitWindowSize(640,480);
-	glutInitWindowPosition(100, 100);
+	glutInitDisplayMode (GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+	glutInitWindowSize (500, 500);
+	glutInitWindowPosition (100, 100);
 	glutCreateWindow("Objek 3D");
-	glutDisplayFunc(displayWire);
-	glClearColor(1.0f, 1.0f, 1.0f,0.0f);  
-	glViewport(0, 0, 640, 480);
+	init ();
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
 	glutMainLoop();
+	return 0;
 }
-*/
- 
-/* 
-* Program transformasi.cpp 
-* Pengenalan OpenGL 
-*/ 
-
- //BALOK// 
-void balok(float x, float y, float z, float a, float b, float c) 
-{ 
- //Sisi Bawah 
-glBegin(GL_QUADS); 
-glVertex2f(10,10); 
-glVertex2f(0,0); 
-glVertex2f(120,0); 
-glVertex2f(110,10); 
-glEnd(); 
- //Sisi Belakang 
-glBegin(GL_QUADS); 
-glVertex2f(10,50); 
-glVertex2f(10,10); 
-glVertex2f(130,10); 
-glVertex2f(130,50); 
-glEnd(); 
- //Sisi Samping Kanan 
-glBegin(GL_QUADS); 
- glVertex2f(120,40); 
-glVertex2f(120,0); 
-glVertex2f(130,10); 
-glVertex2f(130,50); 
-glEnd(); 
- //Sisi Samping Kiri 
-glBegin(GL_QUADS); 
-glColor3f(x,y,z); 
-glVertex2f(0,40); 
-glVertex2f(0,0); 
-glVertex2f(10,10); 
-glVertex2f(10,50); 
-glEnd(); 
- //Sisi Depan 
-glBegin(GL_QUADS); 
-glVertex2f(0,40); 
-glVertex2f(0,0); 
-glVertex2f(120,0); 
-glVertex2f(120,40); 
-glEnd(); 
-//Sisi Atas 
-glBegin(GL_QUADS); 
-glColor3f(a,b,c); 
-glVertex2f(10,50); 
-glVertex2f(0,40); 
-glVertex2f(120,40); 
-glVertex2f(130,50); 
-glEnd(); 
-} 
- void balok2(float d, float e, float f, float g, float h, float i) 
-{ 
- //Sisi Bawah 
-glBegin(GL_QUADS); 
-glColor3f(d,f,g); 
-glVertex2f(10,10); 
-glVertex2f(0,0); 
-glVertex2f(120,0); 
-glVertex2f(130,10); 
-glEnd(); 
- //Sisi Belakang 
-glBegin(GL_QUADS); 
-glColor3f(f,e,f); 
-glVertex2f(10,30); 
-glVertex2f(10,10); 
-glVertex2f(130,10); 
-glVertex2f(130,30); 
-glEnd(); 
- //Sisi Samping Kanan 
-glBegin(GL_QUADS); 
-glColor3f(d,f,f); 
-glVertex2f(120,40); 
-glVertex2f(120,0); 
-glVertex2f(130,10); 
-glVertex2f(130,30); 
-glEnd(); 
- //Sisi Samping Kiri 
-glBegin(GL_QUADS); 
-glColor3f(g,h,i); 
-glVertex2f(0,40); 
-glVertex2f(0,0); 
-glVertex2f(10,10); 
-glVertex2f(10,30); 
-glEnd(); 
-//Sisi Atas 
-glBegin(GL_QUADS); 
-glColor3f(f,g,d); 
-glVertex2f(10,30); 
-glVertex2f(0,40); 
-glVertex2f(120,40); 
-glVertex2f(130,40); 
-glEnd(); 
- //Sisi Depan 
-glBegin(GL_QUADS); 
-glColor3f(d,e,f); 
-glVertex2f(0,40); 
-glVertex2f(0,0); 
-glVertex2f(120,0); 
-glVertex2f(120,40); 
-glEnd(); 
-} 
- void display(void) 
- { 
-/* Membersihkan semua pixel di buffer(clear all pixels) */ 
-glClear (GL_COLOR_BUFFER_BIT); 
- glPushMatrix(); 
-glTranslatef(300.0,300.0,0.0); 
-balok(1.0,0.0,0.0,0,0,1); 
-glPopMatrix(); 
- //Tangan Robot Datar 
-glPushMatrix(); 
-glColor3f(0.6,0.3,0.8); 
-glTranslatef(448.0,312.0,0.0); 
-glRotatef(45.0,0.0,0.0,1.0); 
-balok2(0.1,0.1,0.4,0.6,0.8,0.3); 
-glPopMatrix(); 
- //jari  bawah 
-glColor3f(0.6,0.4,0.8); 
-glPushMatrix(); 
-glTranslatef(532.0,396.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(0.0,0.0,0.0,1.0); 
-balok(0.8,0.7,0.3,2.8,0.8,0.8); 
-glPopMatrix(); 
- //bawah atas 
-glColor3f(0.6,0.4,0.8); 
-glPushMatrix(); 
-glTranslatef(577.0,400.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(50.0,0.0,0.0,1.0); 
-balok2(0.8,0.7,0.3,2.8,0.8,0.8); 
-glPopMatrix(); 
- //jari kedua atas 
-glColor3f(0.6,0.4,0.8); 
-glPushMatrix(); 
-glTranslatef(546.0,439.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(15.0,0.0,0.0,1.0); 
-balok(0.5,0.6,0.3,0.6,0.8,0.2); 
-glPopMatrix(); 
- //jari ketiga atas 
-glColor3f(0.6,0.4,0.8); 
-glPushMatrix(); 
-glTranslatef(535.0,450.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(25.0,0.0,0.0,1.0); 
-balok(0.9,0.5,0.3,0.6,0.4,0.2); 
-glPopMatrix(); 
- glColor3f(0.6,0.4,0.8); 
-glPushMatrix(); 
-glTranslatef(515.0,463.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(30.0,0.0,0.0,1.0); 
-balok(0.9,0.6,0.8,0.4,0.4,0.3); 
-glPopMatrix(); 
- glColor3f(0.6,0.4,0.8); 
-glPushMatrix(); 
-glTranslatef(516.0,415.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(40.0,0.0,0.0,1.0); 
-balok2(0.5,0.6,0.3,0.6,0.8,0.2); 
-glPopMatrix(); 
- glColor3f(0.6,0.4,0.8); 
-glPushMatrix(); 
-glTranslatef(517.0,418.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(60.0,0.0,0.0,1.0); 
-balok2(0.9,0.5,0.3,0.6,0.4,0.2); 
-glPopMatrix(); 
- glColor3f(1.5,1.7,1.3); 
-glPushMatrix(); 
-glTranslatef(519.0,425.0,0.0); 
-glScalef(0.32,0.30,0.0); 
-glRotatef(90.0,0.0,0.0,1.0); 
-balok2(0.9,0.6,0.8,0.4,0.4,0.3); 
-glPopMatrix(); 
- glFlush (); 
-} 
-void init (void) 
-{ 
-// Membuat warna background 
-glClearColor (0.0, 0.0, 0.0, 0.0); 
-/* inisialisasi nilai tampilan (viewing values) */ 
-glMatrixMode(GL_PROJECTION); 
-glLoadIdentity(); // memanggil matriks identitas 
-glOrtho(0.0, 1024.0, 0.0, 700.0, -1.0, 1.0); 
-} 
-/* Deklarasi window size, position, dan display mode(single buffer and RGBA). 
-* Pemanggilan routin inisialisasi. 
-* Memanggil fungsi untuk manampilkan objek di layar 
-*/ 
-int main(int argc, char** argv) 
-{ 
-glutInit(&argc, argv); 
-glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB); 
-glutInitWindowSize (600,400); 
-glutInitWindowPosition (150, 20); 
-glutCreateWindow ("Program Transformasi"); 
-init (); 
-glutDisplayFunc(display); 
-glutMainLoop(); 
-return 0; 
-} 
-
-
